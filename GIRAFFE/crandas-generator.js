b0e0bff35792053135870586fef118b5c449bb3d
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2023-10-27 12:19:32 +0200

    update generator

--- GIRAFFE/crandas-generator.js
+++ GIRAFFE/crandas-generator.js
@@ -2,13 +2,46 @@ module.exports = () => {
   const LANGUAGE = "Crandas";
   const newline = "\n";
 
+  const CLASS_MAP = {
+    ["Table"]: tableCode(node),
+  };
+  // Class list
+  // Table
+  // Join
+  // Column reference
+  // Mean
+  // Comparator
+  // Filter
+
+  function tableCode(node) {
+    const handle = node.parameters.find(
+      (parameter) => parameter.name === "handle"
+    );
+
+    return `${node.name} = cd.get_table("${handle}")`;
+  }
+
   function writePreamble() {
     return `import crandas as cd`;
   }
 
+  function itemToCode(node) {
+    const generator = CLASS_MAP[node.class];
+    if (!generator) {
+      return "";
+    }
+
+    return generator(node);
+  }
+
+  function writeNodes(nodes, links) {
+    return nodes.map((node) => itemToCode(node)).join(newline);
+  }
+
   async function writeCode(nodes, links) {
     const preamble = writePreamble();
-    return [preamble].join(newline);
+    const nodeCode = writeNodes(nodes, links);
+    return [preamble, nodeCode].join(newline);
   }
 
   async function writeFiles(nodes, links) {
